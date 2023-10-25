07f50899a327f5463b0c081f4edecf5b2cdc87fc
tutuwel <12111744@mail.sustech.edu.cn>
2023-10-25 15:09:45 +0800

    pass all case except error

--- phase1/tree.cpp
+++ phase1/tree.cpp
@@ -1,41 +1,62 @@
 #include "tree.hpp"
+
 // 初始化映射
 std::map<std::string, std::string> nameToCategory = {
-        {"STRUCT", "TERMINAL"},
-        {"if", "TERMINAL"},
-        {"else", "TERMINAL"},
-        {"while", "TERMINAL"},
-        {"return", "TERMINAL"},
-        {";", "TERMINAL"},
-        {",", "TERMINAL"},
-        {"==", "TERMINAL"},
-        {"<=", "TERMINAL"},
-        {">=", "TERMINAL"},
-        {"!=", "TERMINAL"},
-        {"=", "TERMINAL"},
-        {"!", "TERMINAL"},
-        {"<", "TERMINAL"},
-        {">", "TERMINAL"},
-        {"+", "TERMINAL"},
-        {"-", "TERMINAL"},
-        {"*", "TERMINAL"},
-        {"/", "TERMINAL"},
-        {"&&", "TERMINAL"},
-        {"||", "TERMINAL"},
-        {"(", "TERMINAL"},
-        {")", "TERMINAL"},
-        {"[", "TERMINAL"},
-        {"]", "TERMINAL"},
-        {"{", "TERMINAL"},
-        {"}", "TERMINAL"},
-        {".", "TERMINAL"},
-        {"TYPE","VAL"},
-        {"CHAR","VAL"},
-        {"ID","VAL"},
-        {}
+    {"STRUCT", "TERMINAL"},
+    {"IF", "TERMINAL"},
+    {"ELSE", "TERMINAL"},
+    {"WHILE", "TERMINAL"},
+    {"RETURN", "TERMINAL"},
+    {"SEMI", "TERMINAL"},
+    {"COMMA", "TERMINAL"},
+    {"EQ", "TERMINAL"},
+    {"LE", "TERMINAL"},
+    {"GE", "TERMINAL"},
+    {"NE", "TERMINAL"},
+    {"ASSIGN", "TERMINAL"},
+    {"NOT", "TERMINAL"},
+    {"LT", "TERMINAL"},
+    {"GT", "TERMINAL"},
+    {"PLUS", "TERMINAL"},
+    {"MINUS", "TERMINAL"},
+    {"MUL", "TERMINAL"},
+    {"DIV", "TERMINAL"},
+    {"AND", "TERMINAL"},
+    {"OR", "TERMINAL"},
+    {"LP", "TERMINAL"},
+    {"RP", "TERMINAL"},
+    {"LB", "TERMINAL"},
+    {"RB", "TERMINAL"},
+    {"LC", "TERMINAL"},
+    {"RC", "TERMINAL"},
+    {"DOT","TERMINAL"},
+    {"TYPE","CHAR"},
+    {"CHAR","CHAR"},
+    {"ID","CHAR"},
+    {"FLOAT","FLOAT"},
+    {"INT","INT"},
+    {"Program", "NONTERMINAL"},
+    {"ExtDefList", "NONTERMINAL"},
+    {"ExtDef", "NONTERMINAL"},
+    {"ExtDecList","NONTERMINAL"},
+    {"Specifier", "NONTERMINAL"},
+    {"StructSpecifier", "NONTERMINAL"},
+    {"VarDec", "NONTERMINAL"},
+    {"FunDec", "NONTERMINAL"},
+    {"VarList", "NONTERMINAL"},
+    {"ParamDec", "NONTERMINAL"},
+    {"CompSt", "NONTERMINAL"},
+    {"StmtList", "NONTERMINAL"},
+    {"Stmt", "NONTERMINAL"},
+    {"DefList", "NONTERMINAL"},
+    {"Def", "NONTERMINAL"},
+    {"DecList", "NONTERMINAL"},
+    {"Dec", "NONTERMINAL"},
+    {"Exp", "NONTERMINAL"},
+    {"Args", "NONTERMINAL"},
     };
 
-Node::Node(string node_name, int nodes_num, int line_num, ...) : name(node_name), nodes_num(0), line_num(0)
+Node::Node(string node_name, int nodes_num, int line_num, ...) : name(node_name), nodes_num(nodes_num), line_num(line_num)
 {
     va_list childnodes;
     va_start(childnodes, line_num);
@@ -66,63 +87,46 @@ Node::Node(string node_name, float float_value) : name(node_name), float_value(f
 }
 
 void printTree(Node *root, int space)
-{
-    printf("%s (%d)\n", root->name.c_str(), root->line_num);
+{   
+    
+    print(root,space);
     int nodes_num = root->nodes_num;
     queue<Node *> nodes_queue = root->com_node;
     for (int i = 0; i < nodes_num; i++)
     {
         Node *node = nodes_queue.front();
-        print(node, space + 2);
+        printTree(node, space + 2);
         nodes_queue.pop();
     }
 }
 
 void print(Node *node, int space)
-{
-    if(node->nodes_num == 0 && node->nodetype == NONTERMINAL){
+{   
+    string print_type=nameToCategory[node->name];
+    if(node->nodes_num == 0 && print_type == "NONTERMINAL"){
         return;
     }
+
     for (int i = 0; i < space; i++)
     {
         printf(" ");
     }
-    switch (node->nodetype)
-    {
-    case Type:
-    {
-        printf("TYPE: %s\n", node->char_value);
-        break;
+    
+    if (print_type=="NONTERMINAL"){
+        std::cout << node->name <<" (" << node->line_num << ")" <<std::endl;
     }
-    case Int:
+    else if (print_type=="CHAR")
     {
-        printf("INT: %s\n", node->name.c_str());
-        break;
+        std::cout << node->name <<": " << node->char_value <<std::endl;
     }
-    case Char:
+    else if (print_type=="INT")
     {
-        printf("CHAR: %s\n", node->char_value);
-        break;
+        std::cout << node->name <<": " << node->int_value <<std::endl;
     }
-    case Float:
-    {
-        printf("FLOAT: %s\n", node->name.c_str());
-        break;
+    else if (print_type=="FLOAT"){
+        std::cout << node->name <<": " << node->float_value <<std::endl;
     }
-    case Id:
-    {
-        printf("ID: %s\n", node->char_value);
-        break;
-    }
-    case TERMINAL:
-    {
-        printf("%s\n", node->name.c_str());
-        break;
-    }
-    case NONTERMINAL:
-    {
-        printTree(node, space);
-        break;
-    }
-    }
+    else if (print_type=="TERMINAL"){
+        std::cout << node->name <<std::endl;
+    }    
 }

417235222d4cbb4029b9765c5720bb6241f68dca
tutuwel <12111744@mail.sustech.edu.cn>
2023-11-28 00:04:56 +0800

    errcheck

--- phase2/err.cpp
+++ phase2/err.cpp
@@ -1,12 +1,12 @@
   #include "err.hpp"
-   void argerr(errtype e,int line,int exp,int actual){
-     if (e==argunmatch)
-        {
-            cout<<"Error type 9 at Line "<<line<<": invalid argument number, except "<<exp<<", got "<<actual<<endl;
-        }
-   }
-   void error(errtype e,int line,char* id){
-        if (e==varnodef){
+
+
+
+
+
+    
+   void err(errtype e,int line,char* id="",int exp=0,int actual=0){
+    if (e==varnodef){
             cout<<"Error type 1 at Line "<<line<<": "<<id<<" is used without a definition"<<endl;
         }else if (e==funnodef){
             cout<<"Error type 2 at Line "<<line<<": "<<id<<" is invoked without a definition"<<endl;
@@ -15,7 +15,8 @@
         }else if (e==funredef)
         {
             cout<<"Error type 4 at Line "<<line<<": "<<id<<" is redefined"<<endl;
-        }else if (e==equnmatch)
+        }
+        else if (e==equnmatch)
         {
             cout<<"Error type 5 at Line "<<line<<": unmatching types appear at both sides of the assignment operator"<<endl;
         }else if (e==rvalleft)
@@ -27,22 +28,28 @@
         }else if (e==returnunmatch)
         {
             cout<<"Error type 8 at Line "<<line<<": incompatiable return type"<<endl;
-        }else if (err==noT&&kind==arr)
+        }
+        else if (e==argunmatch)
         {
-            cout<<"Error type 10 at Line "<<line<<": applying indexing operator ([…]) on non-array type variables"<<endl;
-        }else if (err==noT&&kind==fun)
+            cout<<"Error type 9 at Line "<<line<<": invalid argument number, except "<<exp<<", got "<<actual<<endl;
+            //can add a type check
+        }
+        else if (e==notanarr)
         {
-            cout<<"Error type 11 at Line "<<line<<": applying function invocation operator (foo(…)) on non-function names"<<endl;
-        }else if (err==noT&&kind==index)
+            cout<<"Error type 10 at Line "<<line<<": indexing on non-array variable"<<endl;
+        }else if (e==notafun)
         {
-            cout<<"Error type 12 at Line "<<line<<": array indexing with a non-integer type expression"<<endl;
-        }else if (err==noT&&kind==strucT)
+            cout<<"Error type 11 at Line "<<line<<": invoking non-function variable"<<endl;
+        }else if (e==indexnoint)
         {
-            cout<<"Error type 13 at Line "<<line<<": accessing members of a non-structure variable"<<endl;
-        }else if (err==nohas&&kind==strucT)
+            cout<<"Error type 12 at Line "<<line<<": indexing by non-integer"<<endl;
+        }else if (e==dotnostuct)
+        {
+            cout<<"Error type 13 at Line "<<line<<": accessing with non-struct variable"<<endl;
+        }else if (e==structnohas)
         {
             cout<<"Error type 14 at Line "<<line<<": accessing an undefined structure member"<<endl;
-        }else if (err==redef&&kind==strucT)
+        }else if (e=structredef)
         {
             cout<<"Error type 15 at Line "<<line<<": redefine the same structure type"<<endl;
         } else{

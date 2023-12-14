fc094535a4fbb9d7c965a69c56aef1a3642e56a3
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2023-12-14 23:27:32 +0800

    [Phase3] Change children `list<node*>` to `vector<node*>`

--- phase3/node.cpp
+++ phase3/node.cpp
@@ -1,17 +1,17 @@
 using namespace std;
 
-#include <list>
+#include <vector>
 #include <string>
 
 class node { //syntax tree node
     public:
         string type; //eg: "ID","VarDec","COMMA"
-        list<node*> children;
+        vector<node*> children;
         string value; //only for leaf node: INT, ID
 
     node(string type): node(type, ""){}
 
     node(string type, string value): type(type), value(value){}
 
-    node(string type, list<node*> children): type(type), children(children){}
+    node(string type, vector<node*> children): type(type), children(children){}
 };

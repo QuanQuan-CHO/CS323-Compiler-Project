63aa280a956be719ac2be7be927f7882e68015b3
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2023-12-19 22:40:22 +0800

    [Phase3] Add `spl.cpp` to run spl code

---
+++ phase3/test-base/spl.cpp
@@ -0,0 +1,16 @@
+//For running .spl file to test compiler
+#include <iostream>
+using namespace std;
+
+void write(int a) {
+    cout << a << endl;
+}
+
+int read() {
+    int a;
+    cin >> a;
+    return a;
+}
+
+//---------------------PASTE SPL CODE BELOW---------------------
+
